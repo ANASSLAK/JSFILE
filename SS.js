4c72f9abd7db6fc944a374403f6e6d866de07cd6
LAKTAIBI <50059497+ANASSLAK@users.noreply.github.com>
2021-09-17 19:23:47 +0100

    Create SS.js

---
+++ SS.js
@@ -0,0 +1,15 @@
+console.log('HI FROMfddf GITHUB');
+
+browser.webRequest.onBeforeRequest.addListener( 
+          function(details) { 
+            console.log(details)
+            return {cancel: true}; 
+          },
+          {urls: 
+            ["*://*.doubleclick.net/*",
+            "*://*.googleadservices.com/*",
+            "*://*.googlesyndication.com/*",
+            "*://*.moat.com/*"]
+          },
+          ["blocking"]
+        );
